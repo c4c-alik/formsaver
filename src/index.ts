c2556ea2a53ebf249423110c36016598ed4bf27c
liuyuan <537510672@qq.com>
2026-02-02 17:46:10 +0800

    init

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-// Entry point for the extension
-// This file can export modules or serve as the main entry point for the extension
-
-export { } from './background';
-export { } from './content';
-export { } from './contextMenu';
-
-console.log('FormSaver extension loaded');
