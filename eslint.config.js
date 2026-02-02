a08b18b6367b53ab1eca9b9b8db861fd891a90b5
liuyuan <537510672@qq.com>
2026-02-02 18:51:48 +0800

    add: add eslint, prettier

---
+++ eslint.config.js
@@ -0,0 +1,38 @@
+// @ts-check
+
+import js from '@eslint/js';
+import tsPlugin from 'typescript-eslint';
+import prettierConfig from 'eslint-config-prettier';
+import pluginPreprocessor from 'eslint-plugin-prettier/recommended';
+
+export default tsPlugin.config(
+  js.configs.recommended,
+  ...tsPlugin.configs.recommended,
+  prettierConfig,
+  pluginPreprocessor,
+  {
+    languageOptions: {
+      ecmaVersion: 2023,
+      sourceType: 'module',
+      globals: {
+        chrome: 'readonly'
+      }
+    },
+    rules: {
+      'indent': ['error', 2],
+      'linebreak-style': ['error', 'unix'],
+      'quotes': ['error', 'single'],
+      'semi': ['error', 'always'],
+      '@typescript-eslint/no-unused-vars': 'error',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-non-null-assertion': 'off',
+      '@typescript-eslint/ban-ts-comment': 'off'
+    },
+    files: ['src/**/*.ts'],
+    ignores: ['dist/**', 'node_modules/**', '*.json'],
+    linterOptions: {
+      reportUnusedDisableDirectives: 'error'
+    }
+  }
+);
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+// Entry point for the FormSaver extension
+console.log('FormSaver extension initialized');
